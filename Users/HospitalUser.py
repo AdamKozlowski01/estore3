faf218f9a0585ce46999115298b56a055d22cd54
Vince Zipparro <vincezipparro@gmail.com>
2016-05-12 16:16:19 -0500

    Delete HospitalUser.py

--- Users/HospitalUser.py
+++
@@ -1,9 +0,0 @@
-class HospitalUser(User):
-	def __init__(self):
-		userID = -1
-		firstName = ""
-		lastName = ""
-		shippingAddress = "" #do we need this in the users?
-		email = ""
-		authorized = false
-		group = None
