d0d3ffba0f4762ad487ab09b4df5d3e80ea7304a
Vince Zipparro <vincezipparro@gmail.com>
2016-05-12 16:15:57 -0500

    Delete VendorUser.py

--- Users/VendorUser.py
+++
@@ -1,9 +0,0 @@
-class VendorUser(User):
-		def __init__(self):
-		userID = -1
-		firstName = ""
-		lastName = ""
-		shippingAddress = "" #do we need this in the users?
-		email = ""
-		authorized = false
-		group = None
