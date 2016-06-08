c888363b6b1ed7035446f1ca6852af4e0d3d0649
ajalfieri <ajalfieri@loop-depaulsecure-198-195.depaulsecure-student.depaul.edu>
2016-06-08 16:20:54 -0500

    full chat
    full chat

--- models/db1.py
+++ models/db1.py
@@ -1,20 +1,68 @@
 NE = IS_NOT_EMPTY()
+
+db.define_table(
+    'customer',
+    Field('user_ID',unique = True),
+    Field('firstName'),
+    Field('middleName',required = False),
+    Field('lastName'),
+    Field('profession'),
+    Field('billingAdd_ID'),
+    Field('shippingAdd_ID'),
+    Field('email'),
+    Field('is_Authorized','boolean'),
+    Field('password'),
+    Field('group_ID'),
+    Field('organization_ID'),
+    Field('is_Hospital','boolean')
+    )
+db.define_table(
+    'address',
+    Field('user_ID'),
+    Field('typeofAddress'),
+    Field('streetAddress'),
+    Field('city'),
+    Field('address_state'),
+    Field('zip')
+    )
+db.define_table(
+    'wishList',
+    Field('user_ID'),
+    Field('product_ID')
+    )
+
+db.define_table(
+    'vendors',
+    Field('v_ID',unique = True,required = True),
+    Field('v_Name'),
+    Field('p_Type')
+    )
+db.define_table(
+    'product_Type',
+    Field('pType_ID'),
+    Field('category')
+    )
 db.define_table(
     'product',
     Field('code',requires=NE),
     Field('name',requires=NE),
     Field('description',requires=NE),
-    Field('qty_in_stock'),
+    Field('qty_in_stock','integer'),
     Field('unit_price','decimal(10,2)'),
     Field('image','upload'),
     Field('tags'),
+    Field('category'),
     Field('popularity','integer',default=0),
     Field('featured','boolean',default=False),
     Field('on_sale','boolean',default=False),
+    Field('v_ID', 'reference hospitals'),
     Field('tax','decimal(10,2)'),
     Field('keywords',required=True,
           compute=lambda r: "%(code)s %(name)s %(tags)s" % r),
     auth.signature)
+
+productTable = db['product']
+productTable.v_ID.requires = IS_IN_DB(db, db.hospitals.id,'%(h_Name)s')
 
 db.define_table(
     'purchase_order',
@@ -43,18 +91,67 @@ db.define_table(
     Field('quantity_in_cart','integer'),
     auth.signature)
 
+db.define_table(
+    'conversations',
+    Field('text_file','string'))
+
+db.define_table(
+    'current_conv',
+    Field('conv','string', default=''),
+    auth.signature)
+
 #db(db.auth_user.id>1).delete()
 #db(db.product).delete()
+
+'''
+db.define_table(
+    'product',
+    Field('code',requires=NE),
+    Field('name',requires=NE),
+    Field('description',requires=NE),
+    Field('qty_in_stock','integer'),
+    Field('unit_price','decimal(10,2)'),
+    Field('image','upload'),
+    Field('tags'),
+    Field('category'),
+    Field('popularity','integer',default=0),
+    Field('featured','boolean',default=False),
+    Field('on_sale','boolean',default=False),
+    Field('v_ID', 'reference hospitals'),
+    Field('tax','decimal(10,2)'),
+    Field('keywords',required=True,
+          compute=lambda r: "%(code)s %(name)s %(tags)s" % r),
+    auth.signature)
+'''
 if db(db.product).count()==0:
-    import random
-    from gluon.contrib.populate import populate
-    n = 10000
-    for name in ['Table','Chair','Desk','Shelves','Pen','Robot','Pillow','Bed','Car']:
-        for color in ['red','blue','greem','orange','yellow']:
-            n = n+1
-            db.product.insert(
-                code="%.5i" % n,
-                name=name,tags=name+' '+color,description='bla '*100,
-                unit_price=random.randint(10,1000),tax=0.10,
-                qty_in_stock=random.randint(0,100))
-                             
+    import re
+    import os
+    file = open(os.getcwd()+'/applications/estore3/models/save.txt', 'r')
+    for line in file:
+        l = re.split('\|',line)
+        db.product.insert(
+            code=l[0],
+            name=l[1],
+            description=l[2],
+            qty_in_stock=int(l[3]),
+            unit_price=float(l[4]),
+            image=l[5],
+            tags=l[6],
+            category = l[7],
+            popularity=int(l[8]),
+            featured=int(l[9]),
+            on_sale=int(l[10]),
+            v_ID = l[11],
+            tax=0.10)
+
+response.generic_patterns = ['*'] if request.is_local else []
+from gluon.tools import Auth, prettydate
+auth = Auth(db, hmac_key = Auth.get_or_create_key())
+auth.define_tables()
+
+Post = db.define_table("post",
+                       Field("textmessage","text",requires=IS_NOT_EMPTY(),notnull=True),
+                       auth.signature
+                       )
+Post.is_active_readable = False
+Post.is_active_writable = False
